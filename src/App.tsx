cad7e8ff286092a778e7e72dcb5d13f0bbae2549
BabarAslamm <babaraslam825@gmail.com>
2026-06-17 21:57:24 +0500

    Quiz Pages Added With Routing

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+
+import Home from './pages/Home.jsx';
+import LeaderBoard from './pages/LeaderBoard.jsx';
+import Quiz from './pages/Quiz.jsx';
+
+const Result = lazy(() => import('./pages/Result.jsx'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/leaderboard" element={<LeaderBoard />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
