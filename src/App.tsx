154f7d597b130ed34fdf51ac31ee17cab8fd286e
BabarAslamm <babaraslam825@gmail.com>
2026-06-25 23:48:54 +0500

    QuizContext Added

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,26 @@ import { Suspense, lazy } from 'react';
 import Home from './pages/Home.jsx';
 import LeaderBoard from './pages/LeaderBoard.jsx';
 import Quiz from './pages/Quiz.jsx';
-
 const Result = lazy(() => import('./pages/Result.jsx'));
+
+import { QuizProvider } from './context/QuizContext'
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/leaderboard" element={<LeaderBoard />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </Suspense>
+    <QuizProvider>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/leaderboard" element={<LeaderBoard />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/result" element={<Result />} />
+            </Routes>
+          </Suspense>
     </BrowserRouter>
+    </QuizProvider>
+    
   );
 }
 
---
+++ src/context/QuizContext.jsx
@@ -0,0 +1,20 @@
+import {  createContext, useState } from "react";
+
+const QuizContext = createContext(null);
+
+
+export function QuizProvider({ children }) {
+
+
+  const [name, setName ] = useState('');
+
+  return(
+    <QuizContext.Provider value={{ name, setName}}>
+    { children }
+    </QuizContext.Provider>
+  )
+
+}
+
+
+
