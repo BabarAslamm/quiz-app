5ddd8e36c6d90ad29b16588aa51052f206253634
BabarAslamm <babaraslam825@gmail.com>
2026-09-03 22:16:07 +0500

    Leaderboard Page Completed

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,7 +1,10 @@
 import {useState, useEffect, use} from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 
 function LeaderBoard() {
+
+    const navigate = useNavigate()
 
     const [ data, setData ] = useState([]);
   
@@ -15,35 +18,64 @@ function LeaderBoard() {
 
      console.log('leaderboard', data)
 
-  return (
-    <div className="container text-center py-5">
-        <h2 className="fw-bold mb-4">🏆 Leaderboard</h2>
-        <table className="table table-striped shadow">
-            <thead className="table-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Score</th>
-                    <th>Percentage</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                { data.map((entry, index) => (
-                    <tr key={index}>
-                        <td>{ index + 1 }</td>
-                        <td>{ entry.username }</td>
-                        <td>{ entry.score }</td>
-                        <td>{ entry.percentage }%</td>
-                        <td>{ entry.date }</td>
+    const clearLeaderboard = (e) => {
+
+        localStorage.removeItem('leaderboard');
+        setData([])
+        navigate('/')
+
+    }
+
+    
+
+return (
+    data.length > 0 ? (
+        <div className="container text-center py-5">
+            <h2 className="fw-bold mb-4">🏆 Leaderboard</h2>
+
+            <table className="table table-striped shadow">
+                <thead className="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Score</th>
+                        <th>Percentage</th>
+                        <th>Date</th>
                     </tr>
-                ))}
-                
-            </tbody>
-        </table>
-        <button className="btn btn-danger mt-3">Clear Leaderboard</button>
-    </div>
-  )
+                </thead>
+
+                <tbody>
+                    {data.map((entry, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{entry.username}</td>
+                            <td>{entry.score}</td>
+                            <td>{entry.percentage}%</td>
+                            <td>{entry.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <button className="btn btn-danger mt-3" onClick={clearLeaderboard}>
+                Clear Leaderboard
+            </button>
+        </div>
+    ) : (
+        <div className="container text-center py-5">
+            <h2 className="fw-bold mb-4">🏆 Leaderboard</h2>
+            <p className="text-muted">No leaderboard data available.</p>
+
+            <Link  to="/"  className="btn btn-warning" onClick={() => dispatch({ type:"RESET"})}>
+                Home
+            </Link>
+        </div>
+    )
+);
+
+
+   
+  
 }
 
 export default LeaderBoard
